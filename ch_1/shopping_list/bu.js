ab1ae8ebbf7150ba07f1cb44efea54a8d19dab32
kafka <a50541853@gmail.com>
2018-09-23 15:47:37 +0800

    objectdefinepropery学习

--- ch_1/shopping_list/bu.js
+++ ch_1/shopping_list/bu.js
@@ -1,82 +1,101 @@
 $(document).ready(function () {
-      /**
-       * Add button click handler
-       */
-      function onAdd() {
-        var $ul, li, $li, $label, $div, value;
+  //利用定义属性方法控制dom值
+ 
+  var text = '';
+  var h2 = document.getElementsByTagName('h2')[0];
 
-        value = $('.js-new-item').val();
-        //validate against empty values
-        if (value === '') {
-          return;
-        }
-        $ul = $('ul');
-        $li = $('<li>').appendTo($ul);
-        $div = $('<div>')
-          .addClass('checkbox')
-          .appendTo($li);
-        $label = $('<label>').appendTo($div);
-        $('<input>')
-          .attr('type', 'checkbox')
-          .addClass('item')
-          .attr('name', 'list')
-          .click(toggleRemoved)
-          .appendTo($label);
-        $label
-          .append(value);
-        $('.js-new-item').val('');
-      }
+  Object.defineProperty(obj, 'text', {
+    get: function () {
+      return text;
+    },
+    set: function (newVal) {
+      text = "ccnima";
+      h2.innerHTML = text;
+    }()
+  });
+  /**
+   * Add button click handler
+   */
+  function onAdd() {
+    var $ul, li, $li, $label, $div, value;
 
-      /**
-       * Checkbox click handler - toggles class removed on li parent element
-       * @param ev
-       */
-      function toggleRemoved(ev) {
-        var $el;
+    value = $('.js-new-item').val();
+    //validate against empty values
+    if (value === '') {
+      return;
+    }
+    $ul = $('ul');
+    $li = $('<li>').appendTo($ul);
+    $div = $('<div>')
+      .addClass('checkbox')
+      .appendTo($li);
+    $label = $('<label>').appendTo($div);
+    $('<input>')
+      .attr('type', 'checkbox')
+      .addClass('item')
+      .attr('name', 'list')
+      .click(toggleRemoved)
+      .appendTo($label);
+    $label
+      .append(value);
+    $('.js-new-item').val('');
+  }
 
-        $el = $(ev.currentTarget);
-        $el.closest('li').toggleClass('removed');
-      }
+  /**
+   * Checkbox click handler - toggles class removed on li parent element
+   * @param ev
+   */
+  function toggleRemoved(ev) {
+    var $el;
+
+    $el = $(ev.currentTarget);
+    $el.closest('li').toggleClass('removed');
+  }
 
 
 
-      $('.js-add').click(onAdd);
-      $('.js-item').click(toggleRemoved);
-      $('.js-change-title').keyup(onChangeTitle);
+  $('.js-add').click(onAdd);
+  $('.js-item').click(toggleRemoved);
+  $('.js-change-title').keyup(onChangeTitle);
 
-      new Vue({
-          el: '#app',
-          data: data,
-          methods: {
-            addItem: function () {
-              var text;
-              text = this.newItem.trim();
-              if (text) {
-                this.items.push({
-                  text: text,//所以冒号前的text不用加冒号也不会认成变量咯
-                  checked: false
-                });
-                this.newItem = '';
-              }
-            }
-
+  new Vue({
+    el: '#app',
+    data: data,
+    methods: {
+      addItem: function () {
+        var text;
+        text = this.newItem.trim();
+        if (text) {
+          this.items.push({//新项目推入
+            text: text, //所以冒号前的text不用加冒号也不会认成变量咯
+            checked: false
           });
-      });
+          this.newItem = '';
+        }
+      }
 
-    function onChangeTitle() {
-      $('h2').text($('.js-change-title').val());
     }
 
-    var data = {
-      items: [{
-          text: 'Bananas',
-          checked: true
-        },
-        {
-          text: 'Apples',
-          checked: false
-        }
-      ], //
-      title: 'My Shopping List',
-      newItem: ''
-    };
+
+  });
+});
+
+function onChangeTitle() {
+  $('h2').text($('.js-change-title').val());
+}
+
+var data = {
+  items: [{
+      text: 'Bananas',
+      checked: true
+    },
+    {
+      text: 'Apples',
+      checked: false
+    }
+  ], //
+  title: 'My Shopping List',
+  newItem: ''
+};
+//第二章内容
+var obj = {};//为什么obj一定要放在ready外声明? 
